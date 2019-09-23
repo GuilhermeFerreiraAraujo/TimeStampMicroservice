1669346bc0d728c0b04cc6cd9628be0f6dfdfcd8
Guilherme <guilherme.ferreira28@outlook.com>
2019-09-23 14:25:18 +0200

    Update

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
-// server.js
-// where your node app starts
 
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));  
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/", function (req, res) {
+  const date = new Date();
+  res.json({"unix": date.getTime(), "utc": date.toUTCString()});
 });
 
-
+app.get("/api/timestamp/:date_string", function (req, res) {
+const date_string = req.params.date_string;
+if (isNaN(date_string)) {
+    const date = new Date(date_string );
+    if (date) {
+      var dt = new Date(date_string);
+      res.json({"unix": dt.getTime(), "utc": dt.toUTCString()});
+    } else {
+      console.log(req.params.date_string);
+      res.json({"unix": null, "utc": 'Invalid Date'});
+    }
+} else {
+    const date = new Date(Number(date_string));
+    res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+}
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
